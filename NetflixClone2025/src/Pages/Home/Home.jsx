940a8278c1aa02f481ddf68fd08cc969bbc8d4a1
Abdure-code <alliendalkachew@gmail.com>
2025-02-14 01:08:56 +0300

    updated banner

--- NetflixClone2025/src/Pages/Home/Home.jsx
+++ NetflixClone2025/src/Pages/Home/Home.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Header from '../../Components/Header/Header'
-import Footer from '../../Components/Footer/Footer'
-
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import Banner from "../../Components/Banner/Banner";
 function Home() {
   return (
     <>
-        <Header />
-        <Banner />
-        <Footer />
-
+      <Header />
+      <Banner />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
