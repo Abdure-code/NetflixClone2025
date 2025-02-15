a06c9b2f5b2ebd8bda09a0cd5d33065a542280de
Abdure-code <alliendalkachew@gmail.com>
2025-02-15 20:32:32 +0300

    initiall comit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';init
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/NetflixClone2025/', // <-- Add this line
+});
+
